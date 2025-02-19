cbafa90ccb780472f61d645ceca672b849acb5c6
Alvaro <a.gomez9@sapalomera.cat>
2025-02-19 16:39:39 +0100

    instalar dependencias, separar archivos

--- servidor/game.mjs
+++ servidor/game.mjs
@@ -1,45 +1,31 @@
-// MOVER 
-function movePlayer(player, direction) {
+export function movePlayer(player, direction, players, gameConfig) {
     const step = 10;
-
-    if (checkCollision(player)) {
-        console.log(`🚨 Colisión detectada!`);
-    } else {
+    if (!checkCollision(player, players)) {
         if (direction === 'left') player.x = Math.max(0, player.x - step);
         if (direction === 'right') player.x = Math.min(gameConfig.width, player.x + step);
         if (direction === 'up') player.y = Math.max(0, player.y - step);
         if (direction === 'down') player.y = Math.min(gameConfig.height, player.y + step);
     }
-    
-
-    
 }
 
-function checkCollision(currentPlayer) {
-    Object.values(players).forEach(otherPlayer => {
+function checkCollision(currentPlayer, players, gameConfig) {
+    return Object.values(players).some(otherPlayer => {
         if (currentPlayer.id !== otherPlayer.id) {
-            const distance = Math.hypot(currentPlayer.x - otherPlayer.x, currentPlayer.y - otherPlayer.y);
-            if (distance < 20) { // Distancia mínima para considerar colisión
-                console.log(`⚠️ Colisión detectada entre ${currentPlayer.id} y ${otherPlayer.id}!`);
-                return true;
-            }
-            return false;
+            return Math.hypot(currentPlayer.x - otherPlayer.x, currentPlayer.y - otherPlayer.y) < 20;
         }
+        return false;
     });
 }
 
-function getRandomColor() {
+export function getRandomColor() {
     return `hsl(${Math.random() * 360}, 100%, 50%)`;
 }
 
-function generateRandomRocks() {
+export function generateRandomRocks(config) {
     for (let i = 0; i < 10; i++) {
-        gameConfig.rocks.push({
-            x: Math.random() * gameConfig.width,
-            y: Math.random() * gameConfig.height
-            
+        config.rocks.push({
+            x: Math.random() * config.width,
+            y: Math.random() * config.height
         });
-        console.log('Roca generada en', gameConfig.rocks[i]);
     }
-    broadcastGameState();
 }
